d0cfe86509f85eda69dd8374b8775eaa72c027bb
hruday241 <prasanthhruday@gmail.com>
2021-09-13 10:25:20 +0530

    vare

---
+++ src/test/java/travel/AppTest.java
@@ -0,0 +1,12 @@
+package travel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
